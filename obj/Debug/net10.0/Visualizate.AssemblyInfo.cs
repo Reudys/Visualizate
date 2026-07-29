7c59b8a02ff86e93d0d3e55d4fea299f34002c0c
Reudys <reudysfernando@gmail.com>
2026-07-28 21:31:26 -0400

    News

--- obj/Debug/net10.0/Visualizate.AssemblyInfo.cs
+++ obj/Debug/net10.0/Visualizate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Visualizate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6749ddc16cb5f60a11107636b250d647db5a0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Visualizate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Visualizate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
